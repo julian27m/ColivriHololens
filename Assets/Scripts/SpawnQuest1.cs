f7b3496300e50cf4536b6593d907353145cc434a
Julián Mora <j.morav@uniandes.edu.co>
2023-10-23 16:39:54 -0500

    Quest1 showing position as gem

--- Assets/Scripts/SpawnQuest1.cs
+++ Assets/Scripts/SpawnQuest1.cs
@@ -11,56 +11,53 @@ public class CameraPositionGet
     public float z;
 }
 
-
 public class SpawnQuest1 : MonoBehaviour
 {
     public GameObject objectToSpawn;
-
     private string serverURL = "http://18.188.1.225:8080/data/meta1";
 
     void Start()
     {
-        StartCoroutine(GetCameraPositionFromServer());
+        StartCoroutine(UpdateObjectPosition());
     }
 
-    IEnumerator GetCameraPositionFromServer()
+    IEnumerator UpdateObjectPosition()
     {
-        UnityWebRequest request = UnityWebRequest.Get(serverURL);
-
-        yield return request.SendWebRequest();
-
-        if (request.result == UnityWebRequest.Result.Success)
+        while (true)
         {
-            // Analiza el JSON para obtener la posición
-            string json = request.downloadHandler.text;
-            //Debug.Log("JSON: " + json);
+            UnityWebRequest request = UnityWebRequest.Get(serverURL);
 
-            CameraPositionGet position = JsonUtility.FromJson<CameraPositionGet>(json);
-            //Debug.Log("Position: " + position.x);
+            yield return request.SendWebRequest();
 
-            float xPos = position.x;
-            float yPos = position.y;
-            float zPos = position.z;
-
-            //Debug.Log("Posición: "+xPos+yPos+zPos);
-
-            // Comprueba si la posición es x=0, y=0, z=0
-            if (xPos == 0f && yPos == 0f && zPos == 0f)
+            if (request.result == UnityWebRequest.Result.Success)
             {
-                // La posición es (0, 0, 0), desactiva el objeto
-                objectToSpawn.SetActive(false);
+                string json = request.downloadHandler.text;
+
+                CameraPositionGet position = JsonUtility.FromJson<CameraPositionGet>(json);
+
+                float xPos = position.x;
+                float yPos = position.y;
+                float zPos = position.z;
+
+                if (xPos == 0f && yPos == 0f && zPos == 0f)
+                {
+                    // La posición es (0, 0, 0), desactiva el objeto
+                    objectToSpawn.SetActive(false);
+                }
+                else
+                {
+                    // La posición no es (0, 0, 0), activa el objeto y establece su posición
+                    objectToSpawn.SetActive(true);
+                    objectToSpawn.transform.position = new Vector3(xPos - 9.178f, yPos + 0.2f, zPos - 6.785f);
+                }
             }
             else
             {
-                // La posición no es (0, 0, 0), activa el objeto y establece su posición
-                objectToSpawn.SetActive(true);
-                objectToSpawn.transform.position = new Vector3(xPos-9.178f, yPos+0.2f, zPos-6.785f);
+                Debug.LogError("Error fetching camera position: " + request.error);
             }
-        }
-        else
-        {
-            Debug.LogError("Error fetching camera position: " + request.error);
+
+            // Espera x segundos antes de la próxima actualización
+            yield return new WaitForSeconds(3f);
         }
     }
 }
-
